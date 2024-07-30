cd39b2b23f83b3644b152921a5746d558b67594f
atryknaav <aetryknaavzaaroyt@gmail.com>
2024-07-30 05:58:57 +0300

    Used Redux to manage the shopping cart

--- src/components/ProductCarousel/LeftArrow.tsx
+++ src/components/ProductCarousel/LeftArrow.tsx
@@ -4,7 +4,7 @@ import { BiChevronLeft } from 'react-icons/bi'
 const LeftArrow = () => {
   return (
     <div className='bg-[#44444452] rounded-full relative left-10 top-20'>
-        <BiChevronLeft />HUI
+        <BiChevronLeft />
     </div>
   )
 }
